018dd5d42746ae17ddf67246f57efad061efd7d3
Vladislav Borovikhin <50962824+Xanter410@users.noreply.github.com>
2023-01-22 03:28:40 +0700

    Добавлены 5 уровней

--- Assets/Scripts/GlobalEventHandler.cs
+++ Assets/Scripts/GlobalEventHandler.cs
@@ -2,41 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GlobalEventHandler : MonoBehaviour
 {
     [Header("Global Events")]
-    [SerializeField] private GameEvent _onCoinPickUp;
     [SerializeField] private GameEvent _onPlayerDeaded;
-    [SerializeField] private GameEvent _onLevelCompleted;
 
     private void Start()
     {
-        _onCoinPickUp.AddListener(CoinPickUp);
         _onPlayerDeaded.AddListener(PlayerDeded);
-        _onLevelCompleted.AddListener(LevelCompleted);
     }
 
     private void OnDestroy()
     {
-        _onCoinPickUp.RemoveListener(CoinPickUp);
         _onPlayerDeaded.RemoveListener(PlayerDeded);
-        _onLevelCompleted.RemoveListener(LevelCompleted);
-    }
-
-    private void CoinPickUp()
-    {
-        Debug.Log("Монета была подобрана");
     }
 
     private void PlayerDeded()
     {
-        Debug.Log("Персонаж погиб");
         StartCoroutine(RestartGameWithTimer());
-    }
-
-    private void LevelCompleted()
-    {
-        Debug.Log("Уровень завершен");
     }
 
     IEnumerator RestartGameWithTimer()
